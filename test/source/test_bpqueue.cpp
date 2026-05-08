48dc3aa333d8d5e50a921c56ab724271e69da973
Wai-Shing Luk <luk036@gmail.com>
2026-05-08 22:55:59 +0800

    apply fix-format

--- test/source/test_bpqueue.cpp
+++ test/source/test_bpqueue.cpp
@@ -1,10 +1,10 @@
 #include <doctest/doctest.h>  // for ResultBuilder, CHECK, Expression_lhs
 // #include <__config>            // for std
-#include <cstdint>  // for int32_t, uint32_t
+#include <cstdint>              // for int32_t, uint32_t
 #include <mywheel/bpqueue.hpp>  // for BPQueue
 #include <mywheel/dllist.hpp>   // for Dllink
-#include <utility>  // for pair
-#include <vector>   // for vector
+#include <utility>              // for pair
+#include <vector>               // for vector
 
 using namespace std;
 
